7eaf66143ccc3ed7935f9c386989e5e733900f66
Ricardo Giaviti <ricardo.giaviti@sensedia.com>
2020-06-25 21:59:07 -0300

    Organize

--- censo/ufs-parser/src/main/kotlin/com/gh/rgiaviti/ods/services/CSVService.kt
+++ censo/ufs-parser/src/main/kotlin/com/gh/rgiaviti/ods/services/CSVService.kt
@@ -7,9 +7,6 @@ import org.apache.commons.csv.CSVFormat
 import org.apache.commons.csv.CSVRecord
 import java.io.File
 import java.io.FileReader
-import java.lang.RuntimeException
-import java.nio.file.Path
-import java.nio.file.Paths
 
 object CSVService {
 
